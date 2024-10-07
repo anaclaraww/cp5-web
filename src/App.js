c5523de29e7266f016d91b8d982f0f0e98aea109
Gustavo Jun Ikeda <163008200+Ikeda29@users.noreply.github.com>
2024-10-07 15:03:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import HeroSection from './Components/Hero-Section/Hero-Section';
+import Apresentacao from './Components/Apresentacao/Apresentacao'; 
 import Formcontato from './Components/Formcontato/Formcontato'; 
 import Rodape from './Components/Rodape/Rodape';
 function App() {
   return (
     <div className="App">
       <HeroSection/>
+      <Apresentacao/>
       <Formcontato/>
       <Rodape/>
     </div>

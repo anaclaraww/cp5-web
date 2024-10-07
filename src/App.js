af9ac39a8909ec8eca7ed1722e5709e8ede12b27
NeugeMa <mariana.n.dourado@gmail.com>
2024-10-07 15:19:16 -0300

    Adicionando arquivo de depoimentos + juntanto todas as aplicações :)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import HeroSection from './Components/Hero-Section/Hero-Section';
+import Funcionalidades from './Components/Funcionalidades/Funcionalidades';
+import Depoimentos from './Components/Depoimentos/Depoimentos';
 import Formcontato from './Components/Formcontato/Formcontato'; 
 import Rodape from './Components/Rodape/Rodape';
 function App() {
   return (
     <div className="App">
       <HeroSection/>
+      <Funcionalidades/> 
+      <Depoimentos /> 
       <Formcontato/>
       <Rodape/>
     </div>
